050d64ec0be4f3e94b898600cb7fa74c2b00267e
RaseenaRasak <raseenarasak367@gmail.com>
2024-03-28 16:23:16 +0530

    form

--- Component/Form.js
+++ Component/Form.js
@@ -106,15 +106,17 @@
 
 //----------Creation Form-------------------//
 import React, { useState } from 'react';
-import { View, SafeAreaView, Text, TextInput, StyleSheet, ScrollView, Button } from "react-native";
+import { View, SafeAreaView, Text, TextInput, StyleSheet, ScrollView, Button, TouchableOpacity } from "react-native";
 import { Dropdown } from 'react-native-element-dropdown';
+import { AntDesign } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 
 export default function Form() {
 
-    const [source, setsource] = useState([])
-    const [sales, setsales] = useState([])
+    const [source, setsource] = useState(null)
+    const [sales, setsales] = useState(null)
     const [name, setname] = useState("")
     const [Companyname, setCompanyname] = useState("")
     const [job, setjob] = useState("")
@@ -125,7 +127,7 @@ export default function Form() {
     const [remarks, setremarks] = useState("")
     const [description, setdescription] = useState("")
     const [date, setdate] = useState("")
-    const [Priority, setPriority] = useState("")
+    const [Priority, setPriority] = useState(null)
 
 
     //Source dropdown
@@ -140,98 +142,119 @@ export default function Form() {
         { label: 'Option 1', value: 'option1' },
         { label: 'Option 1', value: 'option1' },
     ]
+    //Priority dropdown
+    const Prioritydrp = [
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 1', value: 'option1' },
+        { label: 'Option 1', value: 'option1' },
+    ]
     return (
         <View>
             <ScrollView>
-                <View style={{ height: 40, width: 360, backgroundColor: "#F42F31", marginTop: 30 }}>
-                    <Text style={{ color: "white", padding: 10 }}>  Add Leads </Text>
+                <View style={{ height: 40, width: 360, backgroundColor: "#F42F31", marginTop: 25 }}>
+                    <Text style={{ color: "white", padding: 10 }}> <AntDesign name="left" size={10} color="white" /> Add Leads </Text>
                 </View>
 
-                <View style={{ marginTop: 30, }}>
-
-
-
-                    <View style={styles.container}>
+                <View style={styles.Container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Source :</Text>
 
                         <Dropdown
-
-
-                            containerStyle={{ width: 200, height: 40 }}
-                            style={{ backgroundColor: '#fafafa' }}
-                            itemStyle={{
-                                justifyContent: 'flex-start'
-                            }}
-                            dropDownStyle={{ backgroundColor: '#fafafa' }}
-
+                            value={source}
+                            onChange={(value) => setsource(value)}
+                            style={styles.input}
+                            data={Sourcedrp}
+                            renderRightIcon={() => (
+                                <AntDesign name="down" size={10} color="#F42F31" />
+                            )}
                         />
+
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Sales Person :</Text>
                         <Dropdown
-                            containerStyle={{ width: 200, height: 40 }}
-                            style={{ backgroundColor: '#fafafa' }}
-                            itemStyle={{
-                                justifyContent: 'flex-start'
-                            }}
-                            dropDownStyle={{ backgroundColor: '#fafafa' }}
-
+                            value={sales}
+                            onChange={(value) => setsales(value)}
+                            style={styles.input}
+                            data={salesperson}
+                            renderRightIcon={() => (
+                                <AntDesign name="down" size={10} color="#F42F31" />
+                            )}
                         />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Contact Name :</Text>
+
                         <TextInput style={styles.input} value={name} onChangeText={setname} placeholder="enter contact name" />
+
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Company Name :</Text>
+
                         <TextInput style={styles.input} value={Companyname} onChangeText={setCompanyname} placeholder="enter company name" multiline />
+
+
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Job Position :</Text>
                         <TextInput style={styles.input} value={job} onChangeText={setjob} placeholder="enter job position" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Phone :</Text>
                         <TextInput style={styles.input} value={phone} onChangeText={setphone} placeholder="enter phone number" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Watsapp Number :</Text>
                         <TextInput style={styles.input} value={watsappnumber} onChangeText={setwatsappnumber} placeholder="enter whatsapp number" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Email :</Text>
                         <TextInput style={styles.input} value={email} onChangeText={setemail} placeholder="enter email" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Address :</Text>
                         <TextInput style={styles.input} value={address} onChangeText={setaddress} placeholder="enter your address" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Remarks :</Text>
-                        <TextInput style={styles.input} value={remarks} onChangeText={setremarks} placeholder="enter remarks" multiline />
+                        <TextInput style={styles.Largeinput} value={remarks} onChangeText={setremarks} placeholder="enter remarks" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Description :</Text>
-                        <TextInput style={styles.input} value={description} onChangeText={setdescription} placeholder="enter description" multiline />
+                        <TextInput style={styles.Largeinput} value={description} onChangeText={setdescription} placeholder="enter description" multiline />
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Expected Completion Date :</Text>
-                        <TextInput style={styles.input} value={date} onChangeText={setdate} placeholder="select expected completion date" multiline />
+                        <View>
+                            <TextInput style={styles.inputdate} value={date} onChangeText={setdate} placeholder="select expected completion date" multilin />
+                            <TouchableOpacity style={styles.calendarIconsectionContainer}>
+                                <Icon name="calendar" size={10} color="#F42F31" />
+                            </TouchableOpacity>
+                        </View>
                     </View>
 
-                    <View style={styles.container}>
+                    <View style={styles.sectionContainer}>
                         <Text style={styles.Textinput}>Priority :</Text>
+                        <Dropdown
+                            value={Priority}
+                            onChange={(value) => setPriority(value)}
+                            style={styles.input}
+                            data={Prioritydrp}
+                            renderRightIcon={() => (
+                                <AntDesign name="down" size={10} color="#F42F31" />
+                            )}
+                        />
                     </View>
 
                     <View style={styles.Button}>
@@ -247,27 +270,62 @@ export default function Form() {
 //stylesheet Api
 const styles = StyleSheet.create({
     Textinput: {
-        marginRight: 20,
-        marginLeft: 10
+        width: 120,
+        marginRight: 10,
+        fontSize: 14,
+        color: "#111111",
+        fontStyle: "italic",
     },
     input: {
         flex: 1,
         borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 5,
+        borderColor: 'gray',
         borderRadius: 5,
-        maxWidth: 200,
-        marginRight: 10
+        padding: 10,
+        fontSize: 11,
+        height: 32,
+        fontStyle: "italic"
     },
-    container: {
+    Largeinput: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        padding: 10,
+        height: 60,
+        fontStyle: "italic"
+    },
+    sectionContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        marginVertical: 10,
+        marginBottom: 10,
     },
     Button: {
         width: 150,
         borderRadius: 2,
-        marginLeft: 100,
-        padding: 30
-    }
+        padding: 10
+    },
+    calendarIconsectionContainer: {
+        position: 'absolute',
+        right: 10,
+        top: '60%',
+        transform: [{ translateY: -10 }],
+    },
+    Container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    inputdate: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        padding: 10,
+        fontSize: 11,
+        height: 32,
+        fontStyle: "italic",
+        minWidth: 200
+    },
 })
