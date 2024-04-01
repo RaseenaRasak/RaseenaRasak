171f357f9cba441f90c70d379f823853628202bf
RaseenaRasak <raseenarasak367@gmail.com>
2024-04-01 19:55:27 +0530

    getapi

--- Component/GetApi.js
+++ Component/GetApi.js
@@ -1,71 +1,101 @@
 //product listing
 
 import { StatusBar } from "expo-status-bar";
-import { View, Text, Image, Button, StyleSheet, ScrollView, FlatList } from "react-native";
+import { View, Text, Image, Button, StyleSheet, ScrollView, FlatList, Dimensions } from "react-native";
 import { SliderBox } from "react-native-image-slider-box";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Carousel, { Pagination } from 'react-native-snap-carousel';
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
+import { ActivityIndicator } from "react-native-paper";
 
+// const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
 export default function App() {
 
   const [data, setdata] = useState([])
+  const [offset, setOffset] = useState(0);
+  const [loadingMore, setLoadingMore] = useState(false);
+
+  const renderLoader = () => {
+    return (
+      <View style={styles.loaderStyle}>
+        <ActivityIndicator size="large" color="#ffa600" />
+      </View>
+    );
+  };
+
+  const loadMoreItem = () => {
+    if (loadingMore) return;
+    setLoadingMore(true);
+    setOffset(offset + 1);
+  };
 
   useEffect(() => {
-    getAPI();
-}, []);
+    if (loadingMore) {
+      getAPI();
+    }
+  }, [offset, loadingMore]);
 
 
   const getAPI = () => {
-    axios({
-      method: "GET",
-      url: "http://139.59.66.191:3016/viewProducts"
-    }).then(res =>{
-      // console.log(res.data);
-      setdata(res.data.data)
-    })
-    .catch(err => console.log(err))
+    axios
+      .get(`http://139.59.66.191:3016/viewProducts?offset=${offset}&limit=4`)
+      .then(res => {
+        // console.log(res.data);
+        setdata(res.data.data)
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        setLoadingMore(false);
+      });
   }
 
   const renderProduct = ({ item }) => {
     return (
-        <View style={styles.card}>
-            {item.image_url && <Image source={{ uri: item.image_url }} style={styles.image} />}
-            {item.product_name && <Text style={styles.title}>{item.product_name}</Text>}
-            {item.cost && <Text style={styles.description}>Cost : $ {item.cost}</Text>}
-        </View>
-    );
-};
-
-const renderTwoProducts = ({ item, index }) => {
-  if (index % 2 === 0) {
-      return (
-          <View style={styles.row}>
-              <View style={styles.cardContainer}>
-                  {renderProduct({ item })}
-              </View>
-              {data[index + 1] && (
-                  <View style={styles.cardContainer}>
-                      {renderProduct({ item: data[index + 1] })}
-                  </View>
-              )}
+      <View >
+        <Text>
+          <View style={styles.card}>
+          {item.image_url && <Image source={{ uri: item.image_url }} style={styles.imageurl} />}
+          {item.product_name && <Text style={styles.title}>{item.product_name}</Text>}
+          {item.cost && <Text style={styles.description}>Cost : $ {item.cost}</Text>}
           </View>
+  
+        </Text>
+      </View>
+
+    );
+  };
+
+  const renderTwoProducts = ({ item, index }) => {
+    if (index % 2 === 0) {
+      return (
+        <View style={styles.row}>
+          <View style={styles.cardContainer}>
+            {renderProduct({ item })}
+          </View>
+          {data[index + 1] && (
+            <View style={styles.cardContainer}>
+              {renderProduct({ item: data[index + 1] })}
+            </View>
+          )}
+        </View>
       );
-  } else {
+    } else {
       return null;
-  }
-};
+    }
+  };
 
 
   const image = [
-    require('./assets/OIP.jpeg'),
-    require('./assets/OIP (1).jpeg'),
-    require('./assets/OIP (2).jpeg')
+    require('../assets/OIP.jpeg'),
+    require('../assets/OIP (1).jpeg'),
+    require('../assets/OIP (2).jpeg')
   ]
-  const logoimg = require("./assets/shansHome.png")
-  const boximage = require("./assets/download.jpeg")
-  const boximage2 = require("./assets/th.jpeg")
-  const boximage3 = require("./assets/th (1).jpeg")
+  const logoimg = require("../assets/shansHome.png")
+  const boximage = require("../assets/download.jpeg")
+  const boximage2 = require("../assets/th.jpeg")
+  const boximage3 = require("../assets/th (1).jpeg")
 
   return (
     <View>
@@ -97,11 +127,36 @@ const renderTwoProducts = ({ item, index }) => {
           </View>
         </View>
 
+        <View style={styles.bannerContainer}>
+
+          {/* <Carousel
+            data={data}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Image source={item.image} style={styles.image} />
+              </View>
+            )}
+            sliderWidth={screenWidth} // Set the slider width to match the screen width
+            itemWidth={screenWidth}   // Set the item width to match the screen width
+            autoplay={true}           // Enable autoplay
+            autoplayInterval={3000}
+            onSnapToItem={(index) => setActiveSlide(index)} // Add this callback to track the active slide
+          /> */}
+          {/* 
+          <Pagination
+            dotsLength={data.length} // Specify the number of dots (should match the number of items in your data array)
+            containerStyle={styles.paginationContainer} // Add your custom styles here
+            dotStyle={styles.paginationDot} // Add your custom dot styles here
+          /> */}
+        </View>
+
         <FlatList
-                data={data}
-                renderItem={renderTwoProducts}
-                keyExtractor={(item) => item._id}
-            />
+          data={data}
+          renderItem={renderTwoProducts}
+          keyExtractor={(item) => item._id}
+          ListFooterComponent={renderLoader}
+          onEndReached={loadMoreItem}
+        />
 
       </ScrollView>
     </View>
@@ -139,37 +194,59 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     shadowColor: '#000',
     shadowOffset: {
-        width: 0,
-        height: 2,
+      width: 0,
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-},
-image: {
+  },
+  imageurl: {
     width: '100%',
     height: 200,
     borderRadius: 10,
     marginBottom: 10,
-},
-title: {
+  },
+  title: {
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 5,
-},
-description: {
+  },
+  description: {
     fontSize: 16,
-},
-container: {
-  flex: 1,
-  padding: 10,
-},
-row: {
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  marginBottom: 10,
-},
-cardContainer: {
-  width: '48%',
-},
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  cardContainer: {
+    width: '48%',
+  },
+  bannerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paginationContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 6,
+    backgroundColor: '#ffa600',
+  },
+  loaderStyle: {
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+
 });
